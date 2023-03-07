58849388dae43e676b998a0715200531df3c634f
WIZ-H4L <wishal572@gmail.com>
2023-03-07 15:31:36 +0700

    selesai

--- mvc/belajar_laravel/resources/views/home.blade.php
+++ mvc/belajar_laravel/resources/views/home.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="jumbotron jumbotron-fluid">
         <div class="container">
-            <h1 class="display-4">Halaman Home</h1>
-            <p class="lead">Halaman ini merupakan halaman Home</p>
+            <h1 class="display-4">Home Page</h1>
+            <p class="lead">This is the Home Page</p>
         </div>
         <p>Nama : {{ $nama }}</p>
         <p>Mata pelajaran :</p>
--- mvc/belajar_laravel/routes/web.php
+++ mvc/belajar_laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ManagementUserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/user',[ManagementUserController::class, 'index']);
+Route::resource('/user', ManagementUserController::class);
 
 Route::get('/home', function () {
     return view('home');
